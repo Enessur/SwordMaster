f96a03455210febf912684b72f157cb91d0fdfd3
Enes Sür <surenesahmet@hotmail.com>
2022-09-30 15:17:47 +0300

    Attack script added to player script

--- SwordMaster/Assets/Scripts/PlayerContoller.cs
+++ SwordMaster/Assets/Scripts/PlayerContoller.cs
@@ -5,22 +5,41 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+
+    public enum TaskCycles
+    {
+        Move,
+        Attack
+    }
     [SerializeField] private float moveSpeed = 60f;
     [SerializeField] private float dashAmount = 50f;
-
+    [SerializeField] private Transform attackPos;
+    [SerializeField] private float attackRange;
+    [SerializeField] private LayerMask whatIsEnemies;
+    [SerializeField] private int damage;
+    [SerializeField] private float attackInterval = 1.5f;
+    [SerializeField] private TaskCycles taskCycle;
+    
     private Rigidbody2D _rb;
     private Vector3 _moveDir;
     private Animator _animator;
     private string _currentAnimation;
     private bool _isDashButtonDown;
+    
+    private float _attackTimer;
+    private bool _canAttack = true;
 
 
-    public SpriteRenderer _renderer;
+   [SerializeField]private SpriteRenderer _renderer;
 
     //Animation States
     const string PLAYER_IDLE = "Idle";
     const string PLAYER_RUN = "Run";
-    
+    const string PLAYER_ATTACK1 = "Attack1";
+    const string PLAYER_ATTACK2 = "Attack2";
+    const string PLAYER_ATTACK3 = "Attack3";
+
+
 
 
     private void Start()
@@ -32,7 +51,37 @@ public class PlayerContoller : MonoBehaviour
 
     void Update()
     {
-        HandleControl();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            taskCycle = TaskCycles.Attack;
+        }
+        else
+        {
+            taskCycle = TaskCycles.Move;
+        }
+        
+        
+        _attackTimer += Time.deltaTime;
+        if (_attackTimer >= attackInterval)
+        {
+            _attackTimer = 0;
+            _canAttack = true;
+        }
+
+         
+        switch(taskCycle)
+        {
+        case TaskCycles.Move:
+            
+            HandleControl();
+            break;
+        
+        case TaskCycles.Attack:
+            
+            Attack();
+            break;
+        }
     }
 
     // add roll and collider to dash 
@@ -49,6 +98,7 @@ public class PlayerContoller : MonoBehaviour
         }
     }
 
+   
     private void HandleControl()
     {
         float moveX = 0f;
@@ -107,5 +157,44 @@ public class PlayerContoller : MonoBehaviour
 
         //play the animation
         _animator.Play(newState);
+    }
+    private void Attack()
+    {
+        //todo:Switch Case / Enum for attack animations
+        int attackNum = 0;
+
+        if (_canAttack)
+        {
+            attackNum += attackNum;
+            Debug.Log("Hit");
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+            }
+            ChangeAnimationState(PLAYER_ATTACK1);
+            
+            //todo : IENUM Animation Controller all animations must end before another starts.
+            // if (attackNum == 0)
+            // {
+            //      ChangeAnimationState(PLAYER_ATTACK1);
+            //      
+            // }
+            // else if (attackNum == 1)
+            // {
+            //     ChangeAnimationState(PLAYER_ATTACK2);
+            //     
+            // }
+            // else
+            // {
+            //     ChangeAnimationState(PLAYER_ATTACK3);
+            //     attackNum = 0;
+            // }
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }
