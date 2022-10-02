bebd6cc29ef223e8ac63c201cde87811f31dfbdf
Enes Sür <surenesahmet@hotmail.com>
2022-10-03 00:43:09 +0300

    fkin animation bug

--- SwordMaster/Assets/Scripts/PlayerContoller.cs
+++ SwordMaster/Assets/Scripts/PlayerContoller.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-
     public enum TaskCycles
     {
         Move,
         Attack
     }
+
     [SerializeField] private float moveSpeed = 60f;
     [SerializeField] private float dashAmount = 50f;
     [SerializeField] private Transform attackPos;
@@ -19,18 +19,20 @@ public class PlayerContoller : MonoBehaviour
     [SerializeField] private int damage;
     [SerializeField] private float attackInterval = 1.5f;
     [SerializeField] private TaskCycles taskCycle;
-    
+    [SerializeField] private float attackDelay = 0.3f;
+
     private Rigidbody2D _rb;
     private Vector3 _moveDir;
     private Animator _animator;
     private string _currentAnimation;
     private bool _isDashButtonDown;
-    
+    private bool _canMove;
     private float _attackTimer;
     private bool _canAttack = true;
+    private float moveX = 0f;
+    private float moveY = 0f;
 
-
-   [SerializeField]private SpriteRenderer _renderer;
+    [SerializeField] private SpriteRenderer _renderer;
 
     //Animation States
     const string PLAYER_IDLE = "Idle";
@@ -38,8 +40,6 @@ public class PlayerContoller : MonoBehaviour
     const string PLAYER_ATTACK1 = "Attack1";
     const string PLAYER_ATTACK2 = "Attack2";
     const string PLAYER_ATTACK3 = "Attack3";
-
-
 
 
     private void Start()
@@ -51,40 +51,31 @@ public class PlayerContoller : MonoBehaviour
 
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
-            taskCycle = TaskCycles.Attack;
-        }
-        else
-        {
-            taskCycle = TaskCycles.Move;
-        }
-        
-        
-        _attackTimer += Time.deltaTime;
-        if (_attackTimer >= attackInterval)
-        {
-            _attackTimer = 0;
-            _canAttack = true;
-        }
-
-         
-        switch(taskCycle)
-        {
-        case TaskCycles.Move:
-            
-            HandleControl();
-            break;
-        
-        case TaskCycles.Attack:
-            
             Attack();
-            break;
+            
         }
+        
+
+        HandleControl();
+
+
+        // switch (taskCycle)
+        // {
+        //     case TaskCycles.Move:
+        //
+        //         HandleControl();
+        //         break;
+        //
+        //     case TaskCycles.Attack:
+        //         Debug.Log("task cycle attack");
+        //         Attack();
+        //         break;
+        // }
     }
 
-    // add roll and collider to dash 
+    // todo: add roll and collider to dash 
     private void FixedUpdate()
     {
         _rb.velocity = _moveDir * moveSpeed;
@@ -98,52 +89,58 @@ public class PlayerContoller : MonoBehaviour
         }
     }
 
-   
+
     private void HandleControl()
     {
-        float moveX = 0f;
-        float moveY = 0f;
-
-        //todo:find a better way to use flipX
-
-        if (Input.GetKey(KeyCode.W))
+        if (_canMove == true)
         {
-            moveY = +1f;
-            _renderer.flipX = !true;
-        }
+           
+            //todo:find a better way to use flipX
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            moveY = -1f;
-            _renderer.flipX = !true;
-        }
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveY = +1f;
+                _renderer.flipX = !true;
+            }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveX = -1f;
-            _renderer.flipX = true;
-        }
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveY = -1f;
+                _renderer.flipX = !true;
+            }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            moveX = +1f;
-            _renderer.flipX = !true;
-        }
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveX = -1f;
+                _renderer.flipX = true;
+            }
 
-        _moveDir = new Vector3(moveX, moveY).normalized;
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveX = +1f;
+                _renderer.flipX = !true;
+            }
 
-        if (moveX != 0 || (moveY != 0))
-        {
-            ChangeAnimationState(PLAYER_RUN);
+            _moveDir = new Vector3(moveX, moveY).normalized;
+
+            if (moveX != 0 || (moveY != 0))
+            {
+                ChangeAnimationState(PLAYER_RUN);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _isDashButtonDown = true;
+            }
         }
         else
         {
-            ChangeAnimationState(PLAYER_IDLE);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            _isDashButtonDown = true;
+            moveX = 0;
+            moveY = 0;
         }
     }
 
@@ -158,13 +155,23 @@ public class PlayerContoller : MonoBehaviour
         //play the animation
         _animator.Play(newState);
     }
+
     private void Attack()
     {
+         
+        _canMove = false;
         //todo:Switch Case / Enum for attack animations
         int attackNum = 0;
-
+        Debug.Log("attack call");
         if (_canAttack)
         {
+            _attackTimer += Time.deltaTime;
+            if (_attackTimer >= attackInterval)
+            {
+                _attackTimer = 0;
+                _canAttack = true;
+            }
+
             attackNum += attackNum;
             Debug.Log("Hit");
             Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
@@ -172,9 +179,11 @@ public class PlayerContoller : MonoBehaviour
             {
                 enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
             }
+
             ChangeAnimationState(PLAYER_ATTACK1);
-            
+
             //todo : IENUM Animation Controller all animations must end before another starts.
+
             // if (attackNum == 0)
             // {
             //      ChangeAnimationState(PLAYER_ATTACK1);
@@ -190,11 +199,18 @@ public class PlayerContoller : MonoBehaviour
             //     ChangeAnimationState(PLAYER_ATTACK3);
             //     attackNum = 0;
             // }
+            Invoke("CanMove", attackDelay);
         }
     }
+
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+    private void CanMove()
+    {
+        _canMove = true;
     }
 }
